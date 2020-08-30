dcc5a0ccefb7db02a5c66db1e24f4cdd4aa97ecc
Nikhil Kadyan <nikhilkadyan@Nikhils-MacBook-Pro.local>
2020-08-30 15:33:54 +0530

    Added Random room id gen to url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 import VideoConference from "./components/VideoConference";
 
 const App = () => {
+  const randomID = Date.now().toPrecision();
     return (
         <Router>
             <Switch>
+              <Redirect from="/" to={`/${randomID}`}  exact/>
                 <Route exec path="/:conId" component={VideoConference} />
             </Switch>
         </Router >
